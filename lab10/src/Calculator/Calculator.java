4c8e8d7c83e239c7ae2ec0a10fd02e38f5acdc12
Josu_Title <138316042+titledontcry@users.noreply.github.com>
2024-09-11 19:03:32 +0700

    Calculator.java

--- lab10/src/Calculator/Calculator.java
+++ lab10/src/Calculator/Calculator.java
@@ -1,170 +1,183 @@
 package Calculator;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javafx.application.Application;
 
 public class Calculator extends JFrame implements ActionListener {
+    private JButton[] numButton = new JButton[10];
+    private JButton[] funcButton = new JButton[7]; 
+    private JButton plus, minus, multi, div, ce, clr, eq;
+    private JTextField display;  
+    private JLabel nowOperate; 
+    private JPanel panel; 
+    private Font font = new Font("Arial", Font.PLAIN, 20);
+    private char operator;  
+    private double num1, num2;  
+    private double result = 0; 
+    private boolean opeCheck;
 
-    // Declaring components as fields
-    private JTextField display;
-    private JButton[] numberButtons = new JButton[10];
-    private JButton[] functionButtons = new JButton[9];
-    private JButton addButton, subButton, mulButton, divButton;
-    private JButton decButton, equButton, delButton, clrButton, negButton;
-    
-    private JPanel panel;
-
-    private Font myFont = new Font("Arial", Font.BOLD, 20);
-    
-    private double num1 = 0, num2 = 0, result = 0;
-    private char operator;
-
-    public Calculator(String title) {
-        super(title);
-        
-        // Setting up the display
+    public Calculator(String name) {
+        super(name);
         display = new JTextField();
-        display.setBounds(50, 25, 300, 50);
-        display.setFont(myFont);
-        display.setEditable(false);
+        display.setBounds(50,25,300,50);
+        display.setFont(font);
+        display.setEditable(false);// ห้ามพิมบนtextfield
         add(display);
-
-        // Creating buttons
+        nowOperate = new JLabel();
+        nowOperate.setBounds(50,100,300,25);
+        nowOperate.setFont(new Font("Arial",Font.PLAIN,14));
+        add(nowOperate);
+       
         for (int i = 0; i < 10; i++) {
-            numberButtons[i] = new JButton(String.valueOf(i));
-            numberButtons[i].addActionListener(this);
-            numberButtons[i].setFont(myFont);
-            numberButtons[i].setFocusable(false);
+            numButton[i] = new JButton(String.valueOf(i));
+            numButton[i].setFont(font); 
+            numButton[i].addActionListener(this); 
+            numButton[i].setFocusable(false);
         }
+        plus = new JButton("+");
+        minus = new JButton("-");
+        multi = new JButton("*");
+        div = new JButton("/");
+        ce = new JButton("CE");
+        clr = new JButton("CLR");
+        eq = new JButton("=");
 
-        // Creating function buttons
-        addButton = new JButton("+");
-        subButton = new JButton("-");
-        mulButton = new JButton("*");
-        divButton = new JButton("/");
-        decButton = new JButton(".");
-        equButton = new JButton("=");
-        delButton = new JButton("Del");
-        clrButton = new JButton("C");
-        negButton = new JButton("(-)");
+        funcButton[0] = plus;
+        funcButton[1] = minus;
+        funcButton[2] = multi;
+        funcButton[3] = div;
+        funcButton[4] = ce;
+        funcButton[5] = clr;
+        funcButton[6] = eq;
 
-        functionButtons[0] = addButton;
-        functionButtons[1] = subButton;
-        functionButtons[2] = mulButton;
-        functionButtons[3] = divButton;
-        functionButtons[4] = decButton;
-        functionButtons[5] = equButton;
-        functionButtons[6] = delButton;
-        functionButtons[7] = clrButton;
-        functionButtons[8] = negButton;
-
-        for (int i = 0; i < 9; i++) {
-            functionButtons[i].addActionListener(this);
-            functionButtons[i].setFont(myFont);
-            functionButtons[i].setFocusable(false);
+        for (int i = 0; i < 7; i++) {
+            funcButton[i].addActionListener(this);
+            funcButton[i].setFont(font);
+            funcButton[i].setFocusable(false);
         }
-
-        // Creating the panel and adding buttons
         panel = new JPanel();
-        panel.setBounds(50, 100, 300, 300);
-        panel.setLayout(new GridLayout(4, 4, 10, 10));
-        
-        // Adding number buttons and function buttons to the panel
-        panel.add(numberButtons[1]);
-        panel.add(numberButtons[2]);
-        panel.add(numberButtons[3]);
-        panel.add(addButton);
-        panel.add(numberButtons[4]);
-        panel.add(numberButtons[5]);
-        panel.add(numberButtons[6]);
-        panel.add(subButton);
-        panel.add(numberButtons[7]);
-        panel.add(numberButtons[8]);
-        panel.add(numberButtons[9]);
-        panel.add(mulButton);
-        panel.add(decButton);
-        panel.add(numberButtons[0]);
-        panel.add(equButton);
-        panel.add(divButton);
+        panel.setBounds(50, 150, 300, 300);
+        panel.setLayout(new GridLayout(5, 4, 10, 10)); 
 
+        // แถว 1
+        panel.add(clr);
+        panel.add(ce);
+        panel.add(div);
+        panel.add(multi);
+        // แถว 2
+        panel.add(numButton[1]);
+        panel.add(numButton[2]);
+        panel.add(numButton[3]);
+        panel.add(plus);
+        // แถว 3
+        panel.add(numButton[4]);
+        panel.add(numButton[5]);
+        panel.add(numButton[6]);
+        panel.add(minus);
+        // แถว 4
+        panel.add(numButton[7]);
+        panel.add(numButton[8]);
+        panel.add(numButton[9]);
+        panel.add(eq);
+        // แถว 5
+        panel.add(numButton[0]);
         add(panel);
-
         setLayout(null);
         setSize(420, 550);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
-
     public void actionPerformed(ActionEvent e) {
         for (int i = 0; i < 10; i++) {
-            if (e.getSource() == numberButtons[i]) {
-                display.setText(display.getText().concat(String.valueOf(i)));
+            if (e.getSource() == numButton[i]) {
+                String currentText = display.getText();
+                if (opeCheck) {
+                    display.setText(String.valueOf(i));
+                    opeCheck = false;
+                } else {
+                    // เพิ่มเลขที่dispaly
+                    display.setText(currentText.concat(String.valueOf(i)));
+                }
+                // update nowOpให้ตรงกับ display
+                if (!opeCheck) {
+                    nowOperate.setText(display.getText());
+                } else {
+                    nowOperate.setText(num1 + " " + operator + " " + display.getText());
+                }
             }
         }
-        if (e.getSource() == decButton) {
-            display.setText(display.getText().concat("."));
+        if (e.getSource() == ce) {
+            display.setText("");
+            nowOperate.setText("");
+            opeCheck = false;
         }
-        if (e.getSource() == addButton) {
+        if (e.getSource() == clr) {
+            display.setText("");
+            nowOperate.setText("");
+            opeCheck = false; // reset all
+        }
+        if (e.getSource() == plus) {
             num1 = Double.parseDouble(display.getText());
+            opeCheck = true;
             operator = '+';
-            display.setText("");
+            display.setText(""); //clear ฟิว
+            nowOperate.setText(num1 + " + ");
         }
-        if (e.getSource() == subButton) {
+        if (e.getSource() == minus) {
             num1 = Double.parseDouble(display.getText());
+            opeCheck = true;
             operator = '-';
-            display.setText("");
+            display.setText(""); 
+            nowOperate.setText(num1 + " - ");
         }
-        if (e.getSource() == mulButton) {
+        if (e.getSource() == div) {
             num1 = Double.parseDouble(display.getText());
-            operator = '*';
-            display.setText("");
-        }
-        if (e.getSource() == divButton) {
-            num1 = Double.parseDouble(display.getText());
+            opeCheck = true;
             operator = '/';
-            display.setText("");
+            display.setText(""); 
+            nowOperate.setText(num1 + " / ");
         }
-        if (e.getSource() == equButton) {
-            num2 = Double.parseDouble(display.getText());
-
-            switch (operator) {
+        if (e.getSource() == multi) {
+            num1 = Double.parseDouble(display.getText());
+            opeCheck = true;
+            operator = '*';
+            display.setText(""); 
+            nowOperate.setText(num1 + " x ");
+        }
+        if (e.getSource() == eq) {
+            num2 = Double.parseDouble(display.getText());  // แปลงค่าจาก displayเป็น num2
+            switch (operator) { 
                 case '+':
-                    result = num1 + num2;
+                    result = num1+num2;
                     break;
                 case '-':
-                    result = num1 - num2;
+                    result = num1-num2;
                     break;
                 case '*':
-                    result = num1 * num2;
+                    result = num1*num2;
                     break;
                 case '/':
-                    result = num1 / num2;
+                    // checkหาร0
+                    if (num2 != 0) {
+                        result = num1/num2;
+                    } else {
+                        display.setText("Error"); 
+                        nowOperate.setText("");
+                        return; 
+                    }
                     break;
             }
-            display.setText(String.valueOf(result));
-            num1 = result;
-        }
-        if (e.getSource() == clrButton) {
-            display.setText("");
-        }
-        if (e.getSource() == delButton) {
-            String str = display.getText();
-            display.setText("");
-            for (int i = 0; i < str.length() - 1; i++) {
-                display.setText(display.getText() + str.charAt(i));
-            }
-        }
-        if (e.getSource() == negButton) {
-            double temp = Double.parseDouble(display.getText());
-            temp *= -1;
-            display.setText(String.valueOf(temp));
+
+            display.setText(String.valueOf(result));  // แสดงผล
+            nowOperate.setText(num1 + " " + operator + " " + num2 + " = " + result);  
+            num1 = result; 
+            opeCheck = false; 
         }
     }
-
     public static void main(String[] args) {
-        new Calculator("Calculator");
+        new Calculator("Mycal");
     }
 }
+
+
